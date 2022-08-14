9b2d0ef3a6d26e3c646455afd6f343a953526467
bonkun0903 <bonkun0903@gmail.com>
2022-08-15 05:54:08 +0900

    初期レンダリング時に猫が表示され、ボタンを押すと猫がランダムに再描画されるように実装

---
+++ src/pages/index.tsx
@@ -0,0 +1,59 @@
+import { useEffect, useState } from "react";
+import type { NextPage, GetServerSideProps } from "next";
+
+interface CatCategory {
+  id: number;
+  name: string;
+}
+
+interface SearchCatImage {
+  breeds: string[];
+  categories: CatCategory[];
+  id: string;
+  url: string;
+  width: number;
+  height: number;
+}
+
+type SearchCatImageResponse = SearchCatImage[];
+
+const fetchCatImage = async () => {
+  const res = await fetch("https://api.thecatapi.com/v1/images/search");
+  const result = (await res.json()) as SearchCatImageResponse;
+  return result[0];
+}
+
+interface IndexPageProps {
+  initialCatImageUrl: string;
+}
+
+const IndexPage: NextPage<IndexPageProps> = ({ initialCatImageUrl }) => {
+  const [catImageUrl, setCatImageUrl] = useState(initialCatImageUrl);
+
+  const handleClick = async () => {
+    const image = await fetchCatImage();
+    setCatImageUrl(image.url)
+  }
+
+  return (
+    <div>
+      <button type="button" onClick={handleClick}>今日のにゃんこ😻</button>
+      <div style={{ marginTop: 8 }}>
+        <img src={catImageUrl} />
+      </div>
+    </div>
+  );
+};
+
+export const getServerSideProps: GetServerSideProps<
+  IndexPageProps
+> = async () => {
+  const catImage = await fetchCatImage();
+  return {
+    props: {
+      initialCatImageUrl: catImage.url 
+    }
+  }
+}
+
+export default IndexPage;
